d7c9c8139d9f0cf2bfa449861142c966861fd47c
Xiaojing <jojoxiaojing@icloud.com>
2018-11-10 13:50:08 -0800

    Add header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
+import Header from './components/header_footer/Header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
     
-          <p>
-              Hello World          
-              </p>
+          <Header>
+                    
+              </Header>
           
         </header>
       </div>
---
+++ src/components/header_footer/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import MenuIcon from '@material-ui/core/Menu';
+import InconButton from '@material-ui/core/IconButton';
+
+class Header extends Component {
+    render() {
+        return {
+            <div>
+            Header
+            </div>
+        };
+    }
+}
+
+export default Header;
